ba36eb107bd59e66692f1ec5a0a47cfe509bde68
Ujang Wahyu <ujangwahyusst@gmail.com>
2018-09-04 10:26:02 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,49 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+
+$router->get('/me', [
+    'middleware' => 'jwt.auth',
+    'uses' => 'UserController@me'
+]);
+
+// Authentication
+$router->group(['prefix' => 'auth'], function ($app) {
+    $app->post('/facebook', 'AuthController@loginFacebook');
+    $app->post('/email', 'AuthController@loginEmail');
+    $app->post('/register', 'AuthController@register');
+    $app->post('/logout', [
+        'middleware' => 'jwt.auth',
+        'uses' => 'AuthController@logout'
+    ]);
+});
+
+$router->group(['prefix' => 'admin', 'middleware' => 'jwt.auth'], function() use ($router) {
+    $router->get('/user', function() {
+        $users = \App\Models\User::all();
+        return response()->json($users);
+    });
+
+    $router->group(['prefix' => 'technology'], function ($app) {
+        $app->get('/', 'TechnologyController@index');
+        $app->get('/{id}', 'TechnologyController@show');
+        $app->post('/', 'TechnologyController@store');
+        $app->post('/{id}', 'TechnologyController@update');
+        $app->delete('/{id}', 'TechnologyController@destroy');
+    });
+});
+
+$router->group(['prefix' => 'daerah'], function ($app) {
+    $app->get('/', 'DaerahController@index');
+    $app->get('/{id}', 'DaerahController@show');
+});
+
+$router->group(['prefix' => 'user'], 
+    function() use ($router) {
+        $router->get('/', function() {
+            $users = \App\Models\User::all();
+            return response()->json($users);
+        });
+    }
+);
