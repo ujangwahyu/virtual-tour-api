484bc33a48ad80ee9b25b212be5c745c8d1af6be
Ujang Wahyu <ujangwahyusst@gmail.com>
2018-09-18 10:35:20 +0700

    (+) Super Admin - get User

--- routes/api/V1.php
+++ routes/api/V1.php
@@ -93,14 +93,14 @@ $router->group(['prefix' => 'admin', 'middleware' => 'jwt.auth'], function() use
 
 $router->group(['prefix' => 'superadmin', 'middleware' => 'jwt.auth'], function() use ($router) {
     $router->get('/user', function() {
-        $users = \App\Models\User::all();
+        $users = \App\Models\User::where('role', 1)->all();
         return response()->json($users);
     });
 
     $router->group(['prefix' => 'register'], function ($app) {
         $app->post('/', 'AuthController@register');
     });
- 
+
 });
 
 $router->group(['prefix' => 'region'], function ($app) {
